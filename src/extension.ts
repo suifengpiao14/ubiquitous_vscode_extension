cf0b7abf3740e27f6ed8d4e601a0195feeddad55
zheng peng <pengzheng@huishoubao.com.cn>
2023-10-07 14:26:16 +0800

    a

--- src/extension.ts
+++ src/extension.ts
@@ -11,31 +11,7 @@ import {
 } from 'vscode-languageclient/node';
 
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	const createApiCommand = vscode.commands.registerCommand(
-		'createapi',
-		() => {
-			const editor = vscode.window.activeTextEditor;
-			if (editor) {
-				const selectedText = editor.document.getText(editor.selection);
-				// 将选中的文本发送给语言服务器
-				client.sendRequest('custom/modifyText', { text: selectedText }).then((modifiedText: any) => {
-					if (modifiedText) {
-						// 如果语言服务器返回了修改后的文本，将其替换当前选中的文本
-						editor.edit((editBuilder) => {
-							editBuilder.replace(editor.selection, modifiedText);
-						});
-					}
-				});
-			}
-		}
-	)
-
-
-	// 注册到监听队列中
-	context.subscriptions.push(createApiCommand)
+function newClient() {
 	const connectionInfo = {
 		port: 8089,
 		host: "127.0.0.1"
@@ -51,7 +27,7 @@ export function activate(context: vscode.ExtensionContext) {
 	};
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		documentSelector: [{ scheme: 'file', language: 'markdown' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
@@ -65,6 +41,35 @@ export function activate(context: vscode.ExtensionContext) {
 		clientOptions
 	);
 	client.start();
+	return client;
+
+}
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	const client = newClient();
+	const createApiCommand = vscode.commands.registerCommand(
+		'extension.createapi',
+		() => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				const selectedText = editor.document.getText(editor.selection);
+				// 将选中的文本发送给语言服务器
+				client.sendRequest('custom/modifyText', { text: selectedText }).then((modifiedText: any) => {
+					if (modifiedText) {
+						// 如果语言服务器返回了修改后的文本，将其替换当前选中的文本
+						editor.edit((editBuilder) => {
+							editBuilder.replace(editor.selection, modifiedText);
+						});
+					}
+				});
+			}
+		}
+	);
+	// 注册到监听队列中
+	context.subscriptions.push(createApiCommand);
 }
 
 // This method is called when your extension is deactivated
